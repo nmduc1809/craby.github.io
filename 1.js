748d02ff756eb1be99506a29f443d2f359b125a8
nmduc1809 <nmduc1809@gmail.com>
2017-06-13 22:45:08 +0700

    second

---
+++ 1.js
@@ -0,0 +1,14 @@
+ $(̣̣̣̣function(){
+ $('#u3861').click(function(event) {
+ 	
+ 	$('.khoinho').addClass('ra');
+ 	$('.khoito').addClass('xuathien');
+ 	/* Act on the event */
+ });
+ $('.khoito').click(function(event) {
+ 	
+ 	$('.khoinho').removeClass('ra');
+ 	$('.khoito').removeClass('xuathien');
+ 	/* Act on the event */
+ });
+ )}
